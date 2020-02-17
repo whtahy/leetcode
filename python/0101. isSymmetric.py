057895ab23bd1fa9c0bf8f94922dd24238889b01
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-02-17 04:14:53 -0500

    Create 0101. isSymmetric.py

---
+++ python/0101. isSymmetric.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# iterative dfs
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return not root or all(x == y for (x, y) in zip(Solution.dfs(root.left, True), Solution.dfs(root.right, False)))
+
+    def dfs(root, left_to_right):
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if left_to_right:
+                    stack.append(node.right)
+                    stack.append(node.left)
+                else:
+                    stack.append(node.left)
+                    stack.append(node.right)
+                yield node.val
+            else:
+                yield node
