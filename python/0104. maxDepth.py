0a0e14522e8619e004392a043be83f339fe39770
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-02-19 01:34:37 -0500

    Create 0104. maxDepth.py

---
+++ python/0104. maxDepth.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# iterative bfs
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        level = 0
+        q = collections.deque([root]) if root else None
+        while q:
+            level += 1
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return level
