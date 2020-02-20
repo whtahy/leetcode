949707c08f3c17b24bbd9f61043ab0af5b4d96ff
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-02-20 00:33:52 -0500

    Create 0107. levelOrderBottom.py

---
+++ python/0107. levelOrderBottom.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        history = []
+        q = collections.deque([root]) if root else []
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            history.append(level)
+        return history[::-1]
