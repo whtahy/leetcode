1b5322fa324a74aae386500311c31f458e4fd109
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-02-23 21:34:46 -0500

    Update 0076. minWindow: speed up

--- python/0076. minWindow.py
+++ python/0076. minWindow.py
@@ -1,35 +1,43 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        set_t = set(t)
-        target_counts = {ch: t.count(ch) for ch in set_t}
-        window_counts = {ch: 0 for ch in set_t}
-
         window = collections.deque()
+        window_counts = {k: -v for k, v in collections.Counter(t).items()}
+
+        counter = 0
         left, right = 0, len(s)
 
         for i, ch in enumerate(s):
-            if ch not in window_counts.keys():
+            if ch not in window_counts:
                 continue
+
+            if window_counts[ch] < 0:
+                counter += 1
 
             window.append(i)
             window_counts[ch] += 1
 
-            if all(window_counts[ch] >= target_counts[ch] for ch in target_counts):
-                l, r = window[0], window[-1]
+            # window not full
+            if counter < len(t):
+                continue
 
-                if r - l < right - left:
-                    left, right = l, r
-                if right - left + 1 == len(t):
-                    break
-
+            while window_counts[s[window[0]]] > 0:
                 window_counts[s[window[0]]] -= 1
                 window.popleft()
 
-            while window_counts[s[window[0]]] > target_counts[s[window[0]]]:
-                window_counts[s[window[0]]] -= 1
-                window.popleft()
+            l, r = window[0], window[-1]
 
-        if right < len(s):
-            return s[left:right+1]
-        else:
+            if r-l < right-left:
+                left, right = l, r
+            if right-left+1 == len(t):
+                break
+
+            # speed up
+            window_counts[s[window[0]]] -= 1
+            if window_counts[s[window[0]]] < 0:
+                counter -= 1
+            window.popleft()
+
+        if right == len(s):
             return ""
+
+        return s[left:right+1]
