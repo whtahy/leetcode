869efa5dce0262cf03be8d545704f2a5b114462a
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-03-12 02:23:12 -0400

    Create 1232. checkStraightLine.py

---
+++ python/1232. checkStraightLine.py
@@ -0,0 +1,18 @@
+# pairwise slopes
+class Solution:
+    def slope(a, b):
+        delta_y = abs(a[1]-b[1])
+        delta_x = abs(a[0]-b[0])
+        if delta_x == 0:
+            return math.inf
+        else:
+            return delta_y / delta_x
+        
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        n = len(coordinates)
+        
+        if n == 2:
+            return True
+        
+        m = Solution.slope(coordinates[0], coordinates[-1])
+        return all(m == Solution.slope(coordinates[i], coordinates[i+1]) for i in range(0, n-1, 2))
