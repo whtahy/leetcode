e850ea91aafa60610aadcb14ee2af64ccdf5cd6c
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-11-21 22:49:23 -0500

    Create 0203. removeElements.py

---
+++ python/0203. removeElements.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        node = head
+        while node and node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        if head and head.val == val:
+            head = head.next
+        return head
