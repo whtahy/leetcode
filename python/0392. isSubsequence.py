7b90a33b0a864325998be88842519719232fc57b
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-11-29 18:34:26 -0500

    Refactor 0392. isSubsequence.py
    Switch to 2 pointer solution.

--- python/0392. isSubsequence.py
+++ python/0392. isSubsequence.py
@@ -1,11 +1,8 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if s == "":
-            return True
-        s = deque(s)
-        for ch in t:
-            if ch == s[0]:
-                s.popleft()
-            if len(s) == 0:
-                return True
-        return False
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
