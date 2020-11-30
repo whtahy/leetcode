841113db3905b0f2038ea7310cc7b29069d3733a
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-11-30 01:20:32 -0500

    Create 0344. reverseString.py

---
+++ python/0344. reverseString.py
@@ -0,0 +1,13 @@
+# built-in
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+
+# 2 pointers
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        i, j = 0, len(s)-1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
