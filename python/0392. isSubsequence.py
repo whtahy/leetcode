f6014821d53001f100499f07af51ead3dae457bd
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-11-29 19:14:19 -0500

    Update 0392. isSubsequence.py
    Add dictionary + binary search solution.

--- python/0392. isSubsequence.py
+++ python/0392. isSubsequence.py
@@ -1,3 +1,4 @@
+# 2 pointers
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         i = j = 0
@@ -6,3 +7,18 @@ class Solution:
                 i += 1
             j += 1
         return i == len(s)
+
+# dictionary + binary search
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        d = defaultdict(list)
+        for i, ch in enumerate(t):
+            d[ch].append(i)
+
+        i = -1
+        for ch in s:
+            j = bisect_left(d[ch], i)
+            if j == len(d[ch]):
+                return False
+            i = d[ch][j] + 1
+        return True
