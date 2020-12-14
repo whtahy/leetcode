a2955123d203b155455ceefe38514fd0077d6db9
Tae Hyon Whang <whtahy@users.noreply.github.com>
2020-12-13 19:07:24 -0500

    Update 0015. threeSum.py

--- python/0015. threeSum.py
+++ python/0015. threeSum.py
@@ -1,25 +1,21 @@
-# credit to @caikehe:
-# https://discuss.leetcode.com/post/24304
 class Solution:
-    def threeSum(self, ls):
-        ls.sort()
-        threes = []
-        _len, prev = len(ls), None
-        for i in range(_len - 2):
-            if sum(ls[i: i + 3]) > 0:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        i = 0
+        while i < len(nums)-2:
+            if sum(nums[i:i+3]) > 0:
                 break
-            elif ls[i] == prev:
-                continue
-            prev = ls[i]
-            l, r = i + 1, _len - 1
-            while l < r:
-                _sum = ls[i] + ls[l] + ls[r]
-                if _sum < 0:
-                    l += 1
-                elif _sum > 0:
-                    r -= 1
+            j, k = i+1, len(nums)-1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j = bisect.bisect_right(nums, nums[j], lo = j+1)
+                    k = bisect.bisect_left(nums, nums[k], hi = k-1)
+                elif s < 0:
+                    j += 1
                 else:
-                    threes += [[ls[i], ls[l], ls[r]]]
-                    l = bisect.bisect_right(ls, ls[l])
-                    r = bisect.bisect_left(ls, ls[r]) - 1
-        return threes
+                    k -= 1
+            i = bisect.bisect_right(nums, nums[i], lo = i+1)
+        return ans
